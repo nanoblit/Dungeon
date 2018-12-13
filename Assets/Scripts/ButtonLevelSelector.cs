20253124d8c2c02d303cb3a5fef63b1ea0b7d3ef
nanoblit <nanoblit@users.noreply.github.com>
2018-12-13 09:05:50 +0100

    Added Menu

---
+++ Assets/Scripts/ButtonLevelSelector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonLevelSelector : MonoBehaviour
+{
+    public int LevelToGoTo;
+
+    public void GoToLevel()
+    {
+        SceneManager.LoadScene(1 + LevelToGoTo);
+    }
+}
---
+++ Assets/Scripts/LevelSelectionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectionController : MonoBehaviour
+{
+    [SerializeField] private Transform _buttonPrefab;
+
+    private void Start()
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+        for (int i = 0; i < sceneCount - 2; i++)
+        {
+            Transform button = Instantiate(_buttonPrefab, transform);
+            button.GetComponentInChildren<Text>().text = (i + 1).ToString();
+            button.GetComponent<ButtonLevelSelector>().LevelToGoTo = i + 1;
+            button.SetAsLastSibling();
+        }
+    }
+}
