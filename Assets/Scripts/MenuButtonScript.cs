315d17f9a65caa75a382d81d5941a58324fae7ec
nanoblit <nanoblit@users.noreply.github.com>
2019-03-20 22:08:59 +0100

    Final

---
+++ Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    public GameObject MenuGO;
+
+    public void SwitchMenu()
+    {
+        if (MenuGO.activeSelf) MenuGO.SetActive(false);
+        else MenuGO.SetActive(true);
+    }
+}
